16ad296d0d73dcd3755504167f63637bef7ce675
waqas <waqas.se2020@gmail.com>
2023-11-06 01:17:38 +0500

    kam kya he thora sa

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function userAge() {
     let result3 = document.querySelector(".result3");
 
     if (input3 > 18) {
-        result3.textContent =  " Old enough";
+        result3.textContent = " Old enough";
     } else {
         result3.textContent = " Too young ";
     }
@@ -49,44 +49,150 @@ function greetingUser() {
     let input4 = document.querySelector("#input4").value;
     // input4 = input4.toUpperCase();
     let result4 = document.querySelector(".result4");
-    
+
     if (input4 === "Waqas") {
-        result4.textContent =  " Welcome to my web technology ";
+        result4.textContent = " Welcome to my web technology ";
     } else {
         result4.textContent = " ";
     }
 
+}
 
-
-    //question No.5
+//question No.5
 
 
 function checkDivideTwo() {
     let input5 = document.querySelector("#input5").value;
     let result5 = document.querySelector(".result5");
-    
-    if (input4 === "Waqas") {
-        result4.textContent =  " Welcome to my web technology ";
+
+    if (input5 === "Waqas") {
+        result5.textContent = " Welcome to my web technology ";
     } else {
-        result4.textContent = " ";
+        result5.textContent = " ";
     }
 
 }
 
+//     //question No.6
 
-//question No.6
 
 
-function userAge() {
-    let input5 = document.querySelector("#input5").value;
-    let result5 = document.querySelector(".result5");
-    
-    if (input4 === "Waqas") {
-        result4.textContent =  " Welcome to my web technology ";
+
+//     //question No.7
+
+function checkOperator() {
+    let input7 = parseFloat(document.querySelector("#input7").value);
+    let inputA7 = parseFloat(document.querySelector("#inputA7").value);
+    let inputOperator7 = document.querySelector("#inputOperator7").value;
+    let result7 = document.querySelector(".result7");
+
+    if (isNaN(input7) || isNaN(inputA7)) {
+        result7.textContent = "Please input valid numbers";
     } else {
-        result4.textContent = " ";
+        switch (inputOperator7) {
+            case '+':
+                result7.textContent = `${input7} and ${inputA7} are being added, the result is ${input7 + inputA7}`;
+                break;
+            case '-':
+                result7.textContent = `${input7} and ${inputA7} are being subtracted, the result is ${input7 - inputA7}`;
+                break;
+            case '*':
+                result7.textContent = `${input7} and ${inputA7} are being multiplied, the result is ${input7 * inputA7}`;
+                break;
+            case '/':
+                if (inputA7 !== 0) {
+                    result7.textContent = `${input7} and ${inputA7} are being divided, the result is ${input7 / inputA7}`;
+                } else {
+                    result7.textContent = "Cannot divide by zero";
+                }
+                break;
+            case '%':
+                if (inputA7 !== 0) {
+                    result7.textContent = `${input7} and ${inputA7} are being operated by modulo, the result is ${input7 % inputA7}`;
+                } else {
+                    result7.textContent = "Cannot perform modulo by zero";
+                }
+                break;
+            default:
+                result7.textContent = "Invalid operator";
+        }
+    }
+}
+
+
+// QUESTION NO. 8
+
+function timeSwitch() {
+    let input8 = document.querySelector("#input8").value;
+    let result8 = document.querySelector(".result8");
+    let time = 1900;
+    let fourZero = 0;
+
+    if (time >= fourZero || time < 1200) {
+        result8.textContent = " Good Morning, Nashta Krlo Friends ";
+    } else if (time >= 1200 || time < 1700) {
+        result8.textContent = " Good AfterNoon, Khana Kha lo Friends ";
+    } else if (time >= 1700 || time < 2100) {
+        result8.textContent = " Good Evening Chai Pee Lo friends ";
+    } else if (time >= 2100 || time < 2359) {
+        result8.textContent = " Good AfterNoon ";
     }
 
 }
+
+// QUESTION NO. 9 not completed 
+
+
+
+
+
+// QUESTION NO. 10 
+
+function passwordMatch() {
+    let input10 = document.querySelector("#input10").value;
+    let inputA10 = document.querySelector("#inputA10").value;
+    let result10 = document.querySelector(".result10");
+
+    if (input10 === inputA10) {
+        result10.textContent = " Correct! The password you entered matches the original password ";
+    } else {
+        alert("please enter your name");
+    }
+
 }
 
+// QUESTION NO. 11
+
+function timeGreetings() {
+    let input12 = parseFloat(document.querySelector("#input12").value);
+    let result12 = document.querySelector(".result12");
+
+    if (input12 < 18) {
+        result12.innerHTML = `<h4> Goood Dayy</h4>`;
+    } else if (input12 >= 18) {
+        result12.innerHTML = ` <h3> GOod Evening </h3>`;
+    } else {
+        alert("please enter time in 24 hour format")
+    }
+
+}
+
+// QUESTION NO. 13
+
+function checkIntegers() {
+    let input13 = parseFloat(document.querySelector("#input13").value);
+    let inputA13 = parseFloat(document.querySelector("#inputA13").value);
+    let result13 = document.querySelector(".result13");
+
+    if (input13 > inputA13) {
+        result13.innerHTML = `<h3>${input13} is greater than ${inputA13}</h3>`;
+    } else if (inputA13 > input13) { // Corrected comparison here
+        result13.innerHTML = `<h3>${input13} is smaller than ${inputA13}</h3>`;
+    } else if (input13 === inputA13 ) {
+        result13.innerHTML = `<h3> both numbers are equal</h3>`;
+        
+    } else {
+        
+        alert("Please enter values in numbers");
+    }
+}
